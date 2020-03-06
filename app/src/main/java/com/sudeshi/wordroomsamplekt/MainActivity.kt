8897ca0ee035d6438c0daf94aa3a4b9b20407b19
perseus98 <perseusmhrn@gmail.com>
2020-03-06 16:34:00 +0530

    Init

---
+++ app/src/main/java/com/sudeshi/wordroomsamplekt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sudeshi.wordroomsamplekt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
